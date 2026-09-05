11cb496f1b3f9f2d27f06551a8a63db4bb981603
Emanuel Herrera <eherrera5@byupathway.edu>
2026-09-05 17:32:37 -0600

    exe3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+
+        int magicNumber = random.Next(1, 101);
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != magicNumber)
+        {
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+
+            Console.Write("Take another guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Yes, the number is " + magicNumber + "! You guessed it!");
     }
 }
