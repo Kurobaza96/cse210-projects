3f7add01689edce4781bf4ecaf7959063e0a5a38
Emanuel Herrera <eherrera5@byupathway.edu>
2026-09-05 17:23:07 -0600

    exe2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,47 @@
 using System;
 
-class Program
+class program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Prompt the user to enter their grade percentage
+        Console.WriteLine("Enter your grade percentage: ");
+        int grade = Convert.parse(Console.ReadLine());
+        string letter;
+
+        // Stores the letter grade in the variable letter based on the grade percentage
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Print the letter grade
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        //Print if the student passed or failed
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed the course. Better luck next time!");
+        }   
     }
 }
