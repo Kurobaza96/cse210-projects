726aff10907c7421301243e9e9141ee42d1ec634
Emanuel Herrera <eherrera5@byupathway.edu>
2026-09-05 17:35:06 -0600

    exe3 notes

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,11 +6,14 @@ class Program
     {
         Random random = new Random();
 
+        // Generate a random number between 1 and 100
         int magicNumber = random.Next(1, 101);
 
+        // First prompt the user to guess
         Console.Write("What is your guess? ");
         int guess = int.Parse(Console.ReadLine());
 
+        // Loop until the user guesses the magic number
         while (guess != magicNumber)
         {
             if (guess < magicNumber)
@@ -22,10 +25,12 @@ class Program
                 Console.WriteLine("Lower");
             }
 
+            // Continue prompting the user for another guess until they get it right
             Console.Write("Take another guess? ");
             guess = int.Parse(Console.ReadLine());
         }
 
+        // If the user guesses the magic number, print a congratulatory message
         Console.WriteLine("Yes, the number is " + magicNumber + "! You guessed it!");
     }
 }
