6a300dfb81ee84024735623fb2c1060fd03a0530
Emanuel Herrera <eherrera5@byupathway.edu>
2026-09-09 22:43:05 -0600

    week2 classesv1.0

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,52 @@
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        if (_entries.Count == 0)
+        {
+            Console.WriteLine("No entries found in the journal.");
+            return;
+        }
+
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine(entry.GetString());
+            }
+        }
+
+        Console.WriteLine("Entries saved to file successfully.");
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+
+        string[] lines = File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            Entry entry = new Entry();
+            entry.LoadFromString(line);
+            _entries.Add(entry);
+        }
+
+        Console.WriteLine("Entries loaded from file successfully.");
+    }
+}
